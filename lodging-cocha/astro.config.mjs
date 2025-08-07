0cdc3bbfed507c3e08b1653303831405cdac2075
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-08-07 10:49:55 -0500

    Add initial project structure with assets, components, layouts and fonts

---
+++ lodging-cocha/astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, fontProviders } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  experimental: {
+    fonts: [
+      {
+        name: 'Poppins',
+        cssVariable: '--font-poppins',
+        provider: fontProviders.fontsource(),
+        weights: [400, 500, 600, 700, 800],
+        styles: ['normal'],
+        subsets: ['latin']
+      },
+      {
+        name: 'Volkhov',
+        cssVariable: '--font-volkhov',
+        provider: fontProviders.fontsource(),
+        weights: [400, 700],
+        styles: ['normal'],
+        subsets: ['latin']
+      }
+    ]
+  },
+  site: '',
+});
