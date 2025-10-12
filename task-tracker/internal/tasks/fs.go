49b7f58dbef8a986f04d6664f17abf9c0e8dba89
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-10-12 18:42:48 -0500

    Add file handling functions for task management

---
+++ task-tracker/internal/tasks/fs.go
@@ -0,0 +1,83 @@
+package tasks
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+)
+
+func tasksFilePath() string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current working directory:", err)
+		return ""
+	}
+	return path.Join(cwd, "data/tasks.json")
+}
+
+func ReadTasksFromFile() ([]Task, error) {
+	filePath := tasksFilePath()
+	dir := path.Dir(filePath)
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		fmt.Println("Error creating directory:", err)
+		return nil, err
+	}
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		fmt.Println("File does not exist. Creating file...")
+		if err := os.WriteFile(filePath, []byte("[]"), 0644); err != nil {
+			fmt.Println("Error creating file:", err)
+			return nil, err
+		}
+		return []Task{}, nil
+	}
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil, err
+	}
+
+	if len(data) == 0 {
+		fmt.Println("File is empty, returning empty task list.")
+		return []Task{}, nil
+	}
+
+	tasks := []Task{}
+	if err := json.Unmarshal(data, &tasks); err != nil {
+		fmt.Println("Error decoding file:", err)
+		fmt.Println("Resetting file to empty array []...")
+		_ = os.WriteFile(filePath, []byte("[]"), 0644)
+		return []Task{}, nil
+	}
+
+	return tasks, nil
+}
+
+func WriteTasksToFile(tasks []Task) error {
+	filePath := tasksFilePath()
+	dir := path.Dir(filePath)
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		fmt.Println("Error creating directory:", err)
+		return err
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return err
+	}
+	defer file.Close()
+
+	if err := json.NewEncoder(file).Encode(tasks); err != nil {
+		fmt.Println("Error encoding file:", err)
+		return err
+	}
+
+	return nil
+}
+
+// 0775 and 0664 permissions are for Unix-based systems. On Windows, these permissions are ignored.
